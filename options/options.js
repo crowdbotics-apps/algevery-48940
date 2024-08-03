5ad92551c545cd929093098743cc06841b47c54a
Crowdbotics <team@crowdbotics.com>
2024-08-03 14:05:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "algevery-48940",
+  url: "https://algevery-48940.botics.co",
+  api: "https://algevery-48940.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
